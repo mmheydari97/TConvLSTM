b468bdd50d64cdd1f43e172c549a453be82da7ac
mmehydari97 <heidarymm@yahoo.com>
2023-05-29 16:11:08 -0400

    Update time_conv_lstm.py

--- torch/time_conv_lstm.py
+++ torch/time_conv_lstm.py
@@ -37,3 +37,49 @@ class TimeAwareConvLSTMCell(nn.Module):
 
         return H, C
     
+
+
+class TConvLSTM(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding, activation, frame_size, return_sequence=False):
+        super(TConvLSTM, self).__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.out_channels = out_channels
+        self.return_sequence = return_sequence
+        
+        self.cell = TimeAwareConvLSTMCell(in_channels, out_channels, kernel_size, padding, activation, frame_size)
+
+    def forward(self, X, durations):
+        batch_size, seq_len, channels, height, width = X.size()
+        
+        output = torch.zeros(batch_size, seq_len, self.out_channels,  
+        height, width, device=self.device)
+        
+        H = torch.zeros(batch_size, self.out_channels, 
+        height, width, device=self.device)
+        
+        C = torch.zeros(batch_size,self.out_channels, 
+        height, width, device=self.device)
+
+        for t, duration in enumerate(durations):
+            H, C = self.cell(X[:, t], H, C, duration)
+            output[:, t, ...] = H
+        
+        if not self.return_sequence:
+            output = torch.squeeze(output[:, -1, ...], dim=1)
+        
+        return output
+
+class TimeAwareConvLSTMAutoencoder(nn.Module):
+    def __init__(self, in_channels, hidden_channels, kernel_size, padding, activation, frame_size):
+        super(TimeAwareConvLSTMAutoencoder, self).__init__()
+        self.encoder = TimeAwareConvLSTMEncoder(in_channels, hidden_channels, kernel_size, padding, activation, frame_size)
+        self.decoder_lstm = nn.LSTM(hidden_channels, hidden_channels, batch_first=True)
+        self.decoder_conv = nn.Conv2d(hidden_channels, in_channels, kernel_size, padding=padding)
+
+    def forward(self, X, durations):
+        H, C = self.encoder(X, durations)
+        H = H.unsqueeze(1)
+        H, _ = self.decoder_lstm(H)
+        H = H.squeeze(1)
+        reconstructed_X = self.decoder_conv(H)
+        return reconstructed_X
